423cc7b6c54bcd45c0d8c06ef8e4fc8fbdca776d
Chris Buchert <chris@smartystreets.com>
2017-11-07 16:19:09 -0700

    A client calls its inner sender's send function when sending a single lookup.

--- tests/us_street/test_client.js
+++ tests/us_street/test_client.js
@@ -1,12 +1,31 @@
 const chai = require("chai");
 const expect = chai.expect;
+const Client = require("../../us_street/client");
+const Lookup = require("../../us_street/lookup");
 
 describe("A client", function () {
 	it ("has a sender.", function () {
-		const Client = require("../../us_street/client");
 		const mockSender = {};
 		const client = new Client(mockSender);
 
 		expect(client.sender).to.deep.equal(mockSender);
+	});
+
+	it ("calls its inner sender's send function.", function () {
+		const mockSender = {
+			send: function(senderLookup) {
+				sentFlag = true;
+				mockSenderLookup = senderLookup;
+			}
+		};
+		const client = new Client(mockSender);
+		let lookup = new Lookup();
+		let sentFlag = false;
+		let mockSenderLookup = {};
+
+		client.sendLookup(lookup);
+
+		expect(sentFlag).to.equal(true);
+		expect(mockSenderLookup).to.deep.equal(lookup);
 	});
 });
--- us_street/client.js
+++ us_street/client.js
@@ -2,6 +2,10 @@ class Client {
 	constructor (sender) {
 		this.sender = sender;
 	}
+
+	sendLookup(lookup) {
+		this.sender.send(lookup);
+	}
 }
 
 module.exports = Client;
