895407e95a438be486f023a15ecd20d5581950b9
Chris Buchert <chris@smartystreets.com>
2017-11-09 16:49:53 -0700

    A batch has an add method.

--- tests/test_batch.js
+++ tests/test_batch.js
@@ -2,11 +2,19 @@ const chai = require("chai");
 const expect = chai.expect;
 const Batch = require("../source/batch");
 
-expect("A batch", function () {
+describe("A batch", function () {
 	it ("has a lookups field.", function () {
 		let batch = new Batch();
 
 		expect(batch.hasOwnProperty("lookups")).to.equal(true);
 		expect(Array.isArray(batch.lookups)).to.equal(true);
+	});
+
+	it ("can add a lookup to its array of lookups.", function () {
+		let batch = new Batch();
+
+		expect(batch.lookups.length).to.equal(0);
+		batch.add("Hi.");
+		expect(batch.lookups.length).to.equal(1);
 	});
 });
