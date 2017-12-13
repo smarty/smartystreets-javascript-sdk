08657e879d0fccaeaaea8327367f6071e037a54f
Chris Buchert <chris@smartystreets.com>
2017-12-13 16:50:15 -0700

    Refactored for cleanliness.

--- tests/test_base_url_sender.js
+++ tests/test_base_url_sender.js
@@ -5,16 +5,21 @@ const Request = require("../source/request");
 const Promise = require("promise");
 
 describe("A Base Url Sender", function () {
-	it("replaces the request's base url with a new value.", function () {
-		let innerSender = {
-			send: function () {
-				return true;
-			}
-		};
-		let request = new Request();
-		let urlOverride = "I'm in your base, killing your mans.";
-		let baseUrlSender = new BaseUrlSender(urlOverride, innerSender);
+	let innerSender;
+	let request;
+	let urlOverride;
+	let baseUrlSender;
 
+	beforeEach(() => {
+		innerSender = {
+			send: () => true
+		};
+		request = new Request();
+		urlOverride = "I'm in your base, killing your mans.";
+		baseUrlSender = new BaseUrlSender(urlOverride, innerSender);
+	});
+
+	it("replaces the request's base url with a new value.", function () {
 		request.baseUrl = "All your baseUrl are belong to us.";
 		baseUrlSender.send(request);
 
@@ -22,17 +27,6 @@ describe("A Base Url Sender", function () {
 	});
 
 	it("returns a promise.", function () {
-		let mockInnerSender = {
-			send: (request) => {
-				return new Promise((resolve, reject) => {
-					resolve(true);
-				});
-			}
-		};
-		let request = new Request();
-		let urlOverride = "Extra crispy.";
-		let baseUrlSender = new BaseUrlSender(urlOverride, mockInnerSender);
-
 		expect(baseUrlSender.send(request) instanceof Promise).to.equal(true);
 	});
 });
