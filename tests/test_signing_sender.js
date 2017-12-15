b08bb33c8b0355fa8f46233451ed55cde37e63a7
Chris Buchert <chris@smartystreets.com>
2017-12-14 17:07:22 -0700

    A signing sender signs with shared credentials.

--- tests/test_signing_sender.js
+++ tests/test_signing_sender.js
@@ -3,20 +3,23 @@ const expect = chai.expect;
 const Request = require("../source/request");
 const SigningSender = require("../source/signing_sender");
 const StaticCredentials = require("../source/static_credentials");
+const SharedCredentials = require("../source/shared_credentials");
 
-describe ("A signing sender", function () {
-	it ("signs a request with static credentials.", function () {
-		let mockAuthId = "testId";
-		let mockAuthToken = "testToken";
+describe("A signing sender", function () {
+	let mockAuthId = "testId";
+	let mockAuthToken = "testToken";
+	let mockHostName = "testHostName";
+	let mockSender = {
+		send: request => new Promise()
+	};
+	let request;
+
+	beforeEach(function () {
+		request = new Request();
+	});
+
+	it("signs a request with static credentials.", function () {
 		let staticCredentials = new StaticCredentials(mockAuthId, mockAuthToken);
-		let mockSender = {
-			send: (request) => {
-				return new Promise((resolve, reject) => {
-					resolve(true);
-				});
-			}
-		};
-		let request = new Request();
 		let signingSender = new SigningSender(staticCredentials, mockSender);
 
 		signingSender.send(request);
@@ -25,5 +28,17 @@ describe ("A signing sender", function () {
 		expect(request.parameters["auth-id"]).to.equal(mockAuthId);
 		expect(request.parameters.hasOwnProperty("auth-token")).to.equal(true);
 		expect(request.parameters["auth-token"]).to.equal(mockAuthToken);
+	});
+
+	it("signs a request with shared credentials.", function () {
+		let sharedCredentials = new SharedCredentials(mockAuthId, mockHostName);
+		let signingSender = new SigningSender(sharedCredentials, mockSender);
+
+		signingSender.send(request);
+
+		expect(request.parameters.hasOwnProperty("auth-id")).to.equal(true);
+		expect(request.parameters["auth-id"]).to.equal(mockAuthId);
+		expect(request.headers.hasOwnProperty("Referer")).to.equal(true);
+		expect(request.headers["Referer"]).to.equal(mockHostName);
 	});
 });
