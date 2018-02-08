30f78e162d3a31ff935fd2d687357eecffd02fdb
Chris Buchert <chris@smartystreets.com>
2018-02-08 16:14:08 -0700

    Refactored tests to remove redundant code.

--- source/international_street/lookup.js
+++ source/international_street/lookup.js
@@ -17,14 +17,26 @@ class Lookup {
 		this.geocode = undefined;
 		this.language = undefined;
 		this.inputId = undefined;
-
-		this.ensureEnoughInfo = this.ensureEnoughInfo.bind(this);
 	}
 
 	ensureEnoughInfo() {
-		if (typeof this.country === "undefined") {
-			throw new UnprocessableEntityError("Country field is required.");
-		}
+		let fieldIsMissing = (field) => {
+			if (!field) return true;
+
+			return field.replace(/\s/g, "").length > 1;
+		};
+
+		let fieldIsSet = (field) => {
+			return !fieldIsMissing(field);
+		};
+
+		if (fieldIsMissing(this.country)) throw new UnprocessableEntityError("Country field is required.");
+
+		if (fieldIsSet(this.freeform)) return true;
+
+		if (fieldIsMissing(this.address1)) throw new UnprocessableEntityError("Either freeform or address1 is required.");
+
+		if (fieldIsMissing(this.locality) || fieldIsMissing(this.administrativeArea)) throw new UnprocessableEntityError("Insufficient information: One or more required fields were not set on the lookup.");
 	}
 }
 
--- tests/international_street/test_lookup.js
+++ tests/international_street/test_lookup.js
@@ -12,15 +12,36 @@ describe("An International Street lookup", function () {
 	});
 
 	it("rejects lookups without a country.", function () {
-		let lookup = new Lookup();
-		let expectedError = new errors.UnprocessableEntityError("Country field is required.");
+		const message = "Country field is required.";
+
+		testRejection(new Lookup(), message);
+	});
+
+	it("rejects lookups with only a country.", function () {
+		const message = "Either freeform or address1 is required.";
+
+		testRejection(new Lookup("a"), message);
+	});
+
+	it("rejects lookups with only a country and address 1.", function () {
+		let lookup = new Lookup("a");
+		lookup.address1 = "b";
+
+		const message = "Insufficient information: One or more required fields were not set on the lookup.";
+
+		testRejection(lookup, message);
+	});
+
+	function testRejection(lookup, message) {
+		let expectedError = new errors.UnprocessableEntityError(message);
 
 		try {
 			lookup.ensureEnoughInfo();
+			expect(true).to.equal(false);
 		}
 		catch (error) {
 			expect(error.message).to.equal(expectedError.message);
 			expect(error).to.be.an.instanceOf(errors.UnprocessableEntityError);
 		}
-	});
+	}
 });
