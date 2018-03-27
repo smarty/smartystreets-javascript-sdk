52d35f3b3bc04528b8d066c6f5029eb82ca4de42
Chris Buchert <chris@smartystreets.com>
2018-03-26 22:20:12 -0600

    Refactored prototype methods only used internally to be private functions within parent prototype methods in the US Street Client.

--- source/us_street/Client.js
+++ source/us_street/Client.js
@@ -18,54 +18,54 @@ class Client {
 		return this.sendBatch(batch);
 	}
 
-	generateRequestPayload(batch) {
-		return batch.lookups.map(lookup => {
-			let inputData = new InputData(lookup);
-
-			inputData.add("street", "street");
-			inputData.add("street2", "street2");
-			inputData.add("secondary", "secondary");
-			inputData.add("city", "city");
-			inputData.add("state", "state");
-			inputData.add("zipcode", "zipCode");
-			inputData.add("lastline", "lastLine");
-			inputData.add("addressee", "addressee");
-			inputData.add("urbanization", "urbanization");
-			inputData.add("match", "match");
-			inputData.add("candidates", "maxCandidates");
-
-			return inputData.data;
-		});
-	}
-
 	sendBatch(batch) {
 		if (batch.isEmpty()) throw new errors.BatchEmptyError;
 
 		let request = new Request();
 
-		if (batch.length() === 1) request.parameters = this.generateRequestPayload(batch)[0];
-		else request.payload = JSON.stringify(this.generateRequestPayload(batch));
+		if (batch.length() === 1) request.parameters = generateRequestPayload(batch)[0];
+		else request.payload = JSON.stringify(generateRequestPayload(batch));
 
 		return new Promise((resolve, reject) => {
 			this.sender.send(request)
 				.then(response => {
 					if (response.error) reject(response.error);
 
-					resolve(this.assignCandidatesToLookups(batch, response));
+					resolve(assignCandidatesToLookups(batch, response));
 				})
 				.catch(reject);
 		});
-	}
 
-	assignCandidatesToLookups(batch, response) {
-		response.payload.map(rawCandidate => {
-			let candidate = new Candidate(rawCandidate);
-			let lookup = batch.getByIndex(candidate.inputIndex);
+		function generateRequestPayload(batch) {
+			return batch.lookups.map(lookup => {
+				let inputData = new InputData(lookup);
 
-			lookup.result.push(candidate);
-		});
+				inputData.add("street", "street");
+				inputData.add("street2", "street2");
+				inputData.add("secondary", "secondary");
+				inputData.add("city", "city");
+				inputData.add("state", "state");
+				inputData.add("zipcode", "zipCode");
+				inputData.add("lastline", "lastLine");
+				inputData.add("addressee", "addressee");
+				inputData.add("urbanization", "urbanization");
+				inputData.add("match", "match");
+				inputData.add("candidates", "maxCandidates");
 
-		return batch;
+				return inputData.data;
+			});
+		}
+
+		function assignCandidatesToLookups(batch, response) {
+			response.payload.map(rawCandidate => {
+				let candidate = new Candidate(rawCandidate);
+				let lookup = batch.getByIndex(candidate.inputIndex);
+
+				lookup.result.push(candidate);
+			});
+
+			return batch;
+		}
 	}
 }
 
