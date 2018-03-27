fa0929357aee785a4aa5d7298f810439047407c6
Chris Buchert <chris@smartystreets.com>
2018-03-26 22:21:40 -0600

    Refactored prototype methods only used internally to be private functions within parent prototype methods in the US Autocomplete Client.

--- source/us_autocomplete/Client.js
+++ source/us_autocomplete/Client.js
@@ -12,39 +12,39 @@ class Client {
 		if (typeof lookup === "undefined") throw new errors.UndefinedLookupError();
 
 		let request = new Request();
-		request.parameters = this.buildRequestParameters(lookup);
+		request.parameters = buildRequestParameters(lookup);
 
 		return new Promise((resolve, reject) => {
 			this.sender.send(request)
 				.then(response => {
 					if (response.error) reject(response.error);
 
-					lookup.result = this.buildSuggestionsFromResponse(response.payload);
+					lookup.result = buildSuggestionsFromResponse(response.payload);
 					resolve(lookup);
 				})
 				.catch(reject);
 		});
-	}
 
-	buildRequestParameters(lookup) {
-		return {
-			prefix: lookup.prefix,
-			suggestions: lookup.maxSuggestions,
-			city_filter: joinFieldWith(lookup.cityFilter, ","),
-			state_filter: joinFieldWith(lookup.stateFilter, ","),
-			prefer: joinFieldWith(lookup.prefer, ";"),
-			prefer_ratio: lookup.preferRatio,
-			geolocate: lookup.geolocate,
-			geolocate_precision: lookup.geolocatePrecision,
-		};
+		function buildRequestParameters(lookup) {
+			return {
+				prefix: lookup.prefix,
+				suggestions: lookup.maxSuggestions,
+				city_filter: joinFieldWith(lookup.cityFilter, ","),
+				state_filter: joinFieldWith(lookup.stateFilter, ","),
+				prefer: joinFieldWith(lookup.prefer, ";"),
+				prefer_ratio: lookup.preferRatio,
+				geolocate: lookup.geolocate,
+				geolocate_precision: lookup.geolocatePrecision,
+			};
 
-		function joinFieldWith(field, delimiter) {
-			if (field.length) return field.join(delimiter);
+			function joinFieldWith(field, delimiter) {
+				if (field.length) return field.join(delimiter);
+			}
 		}
-	}
 
-	buildSuggestionsFromResponse(payload) {
-		return payload.suggestions.map(suggestion => new Suggestion(suggestion));
+		function buildSuggestionsFromResponse(payload) {
+			return payload.suggestions.map(suggestion => new Suggestion(suggestion));
+		}
 	}
 }
 
