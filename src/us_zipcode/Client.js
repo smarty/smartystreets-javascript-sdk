2d91e159829f0d810a253eeaa7132e11c881f047
Chris Buchert <chris@smartystreets.com>
2018-04-05 10:22:24 -0600

    Refactored batch sending out of `us_zipcode` client.

--- src/us_zipcode/Client.js
+++ src/us_zipcode/Client.js
@@ -1,10 +1,10 @@
-const Request = require("../Request");
-const Batch = require("../Batch");
 const Lookup = require("./Lookup");
-const InputData = require("../InputData");
 const Result = require("./Result");
+
+const Batch = require("../Batch");
 const Errors = require("../errors");
-const Promise = require("promise");
+
+const sendBatch = require("../util/sendBatch");
 
 class Client {
 	constructor(sender) {
@@ -22,49 +22,9 @@ class Client {
 		if (dataIsLookup) {
 			batch = new Batch();
 			batch.add(data);
-		} else {
-			batch = data;
-		}
+		} else batch = data;
 
-		if (batch.isEmpty()) throw new Errors.BatchEmptyError;
-
-		let request = new Request();
-
-		if (batch.length() === 1) request.parameters = generateRequestPayload(batch)[0];
-		else request.payload = generateRequestPayload(batch);
-
-		return new Promise((resolve, reject) => {
-			this.sender.send(request)
-				.then(response => {
-					if (response.error) reject(response.error);
-
-					resolve(assignResultsToLookups(batch, response));
-				})
-				.catch(reject);
-		});
-
-		function generateRequestPayload(batch) {
-			return batch.lookups.map((lookup) => {
-				let inputData = new InputData(lookup);
-
-				inputData.add("city", "city");
-				inputData.add("state", "state");
-				inputData.add("zipcode", "zipCode");
-
-				return inputData.data;
-			});
-		}
-
-		function assignResultsToLookups(batch, response) {
-			response.payload.map(rawResult => {
-				let result = new Result(rawResult);
-				let lookup = batch.getByIndex(result.inputIndex);
-
-				lookup.result.push(result);
-			});
-
-			return batch;
-		}
+		return sendBatch(batch, this.sender, Result);
 	}
 }
 
