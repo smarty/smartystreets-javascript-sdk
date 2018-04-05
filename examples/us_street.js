68ec70b33dd2ec896355d3a2a92ec7957ec5b20c
Chris Buchert <chris@smartystreets.com>
2018-04-05 15:40:46 -0600

    Cleaned up examples.

--- examples/us_street.js
+++ examples/us_street.js
@@ -6,21 +6,16 @@ let authId = process.env.SMARTY_AUTH_ID;
 let authToken = process.env.SMARTY_AUTH_TOKEN;
 
 let clientBuilder = new SmartyStreetsCore.ClientBuilder(new SmartyStreetsCore.StaticCredentials(authId, authToken));
-//Configure the Client Builder
 let client = clientBuilder.buildUsStreetApiClient();
 
 let lookup1 = new Lookup();
 lookup1.street = "330 N 100 W";
-// lookup1.city = "Provo";
-// lookup1.state = "Utah";
 lookup1.zipCode = "84601";
 
 let lookup2 = new Lookup();
 lookup2.street = "1600 Amphitheater Pkwy";
 lookup2.city = "Mountainview";
 lookup2.state = "CA";
-
-// client.sendLookup(lookup1).then(() => console.log(), error => console.log());
 
 let batch = new SmartyStreetsCore.Batch();
 batch.add(lookup1);
@@ -30,8 +25,8 @@ client.send(batch)
 	.then(handleSuccess)
 	.catch(handleError);
 
-function handleSuccess() {
-	console.log(lookup1.result);
+function handleSuccess(response) {
+	response.lookups.map(lookup => console.log(lookup.result));
 }
 
 function handleError(response) {
