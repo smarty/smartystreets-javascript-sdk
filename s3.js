87af36c9ceb2478c417c388a4b4453ba0f4f70f4
Chris Buchert <chris@smartystreets.com>
2018-04-20 16:10:44 -0600

    `s3.js` uploads minified and unminified Browserified bundles to S3.

--- s3.js
+++ s3.js
@@ -1,32 +1,39 @@
 const fs = require("fs");
 const zlib = require("zlib");
-
-let AWS = require("aws-sdk");
+const version = require("./package").version;
 const credentials = {
 	accessKeyId: process.env.AWS_ACCESS_KEY,
 	secretAccessKey: process.env.AWS_SECRET_KEY,
 };
+
+let AWS = require("aws-sdk");
 AWS.config.update(credentials);
 
-const s3Stream = require("s3-upload-stream")(new AWS.S3());
-const filesToUpload = __dirname + "/dist/*.min.js";
+const S3Stream = require("s3-upload-stream");
+const fileName = "smartystreets-sdk-" + version;
+const basePath = __dirname + "/dist/";
 
-let read = fs.createReadStream(filesToUpload);
-let compress = zlib.createGzip();
-let upload = s3Stream.upload({
-	Bucket: "",
-	Key: "",
-	ACL: "",
-	StorageClass: "",
-	ContentType: "",
-});
+[".js", ".min.js"].map(fileExtension => uploadFileToS3(basePath, fileName + fileExtension));
 
-upload.on("error", e => {
-	throw new e
-});
-upload.on("part", console.log);
-upload.on("uploaded", console.log);
+function uploadFileToS3(filePath, fileName) {
+	let s3Stream = S3Stream(new AWS.S3());
+	let read = fs.createReadStream(filePath + fileName);
+	let compress = zlib.createGzip();
+	let upload = s3Stream.upload({
+		Bucket: "static.smartystreets.com",
+		Key: "sdk/" + version + "/" + fileName,
+		StorageClass: "STANDARD",
+		ContentType: "application/javascript",
+		ContentEncoding: "gzip",
+	});
 
-read
-	.pipe(compress)
-	.pipe(upload);
+	upload.on("error", e => {
+		throw new e
+	});
+	upload.on("part", console.log);
+	upload.on("uploaded", console.log);
+
+	read
+		.pipe(compress)
+		.pipe(upload);
+}
