b472dfe2e9d4e813d940be711c8e9cd690960492
Chris Buchert <chris@smartystreets.com>
2018-09-24 10:38:38 -0600

    US Autocomplete example uses shared credentials.

--- examples/us_autocomplete.js
+++ examples/us_autocomplete.js
@@ -2,10 +2,9 @@ const SmartyStreetsSDK = require("../index");
 const SmartyStreetsCore = SmartyStreetsSDK.core;
 const Lookup = SmartyStreetsSDK.usAutocomplete.Lookup;
 
-let authId = process.env.SMARTY_AUTH_ID;
-let authToken = process.env.SMARTY_AUTH_TOKEN;
+let websiteKey = "put your website key here.";
 
-let clientBuilder = new SmartyStreetsCore.ClientBuilder(new SmartyStreetsCore.StaticCredentials(authId, authToken));
+let clientBuilder = new SmartyStreetsCore.ClientBuilder(new SmartyStreetsCore.SharedCredentials(websiteKey));
 let client = clientBuilder.buildUsAutocompleteClient();
 let lookup = new Lookup("1080 Pasito");
 
