a7f10856e234fd7955174aecbb3e25a2ee822bd1
Raya Esplin <rayae@byu.edu>
2022-11-01 11:32:24 -0600

    deletes comments

--- src/RetrySender.js
+++ src/RetrySender.js
@@ -12,28 +12,25 @@ class RetrySender {
 		let response = this.inner.send(request);
 
 		for (let i = 0; i < this.maxRetries; i++) {
-			// break if the retry shouldn't handle the response
+
 			if (!this.statusToRetry.includes(parseInt(response.statusCode))) {
 				break;
 			}
-			// is this a 429?
+
 			if (parseInt(response.statusCode) === this.statusTooManyRequests) {
 				let secondsToBackoff = 10;
-				// look for the retry after seconds
 				if(response.headers) {
 					if (parseInt(response.headers["Retry-After"])) {
 						secondsToBackoff = parseInt(response.headers["Retry-After"]);
 					}
 				}
-				// try again, 429 case
 				this.rateLimitBackOff(secondsToBackoff)
 			} else {
-				// not a 429
 				this.backoff(i)
 			}
-			//try sending again
 			response = this.inner.send(request);
 		}
+
 		return response;
 	};
 
