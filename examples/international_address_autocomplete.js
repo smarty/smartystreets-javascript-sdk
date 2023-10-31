8b8a1b81fdcdcb29582b7005ac913f8be63dfc89
Mike Skonnard <mike.skonnard@smarty.com>
2023-10-31 16:38:19 -0600

    reverts local environment changes committed by mistake in the example file, improves comments

--- examples/international_address_autocomplete.js
+++ examples/international_address_autocomplete.js
@@ -1,8 +1,9 @@
-const SmartySDK = require("../index.js");
+const SmartySDK = require("smartystreets-javascript-sdk");
 const SmartyCore = SmartySDK.core;
 const Lookup = SmartySDK.internationalAddressAutocomplete.Lookup;
 
-// US Autocomplete Pro only supports using Embedded Keys
+// US Autocomplete Pro supports Embedded Keys and Secret Keys
+// Remember to be careful how you use "secret key" authentication. See our authentication documentation (https://www.smarty.com/docs/cloud/authentication).
 let key = process.env.SMARTY_EMBEDDED_KEY;
 const credentials = new SmartyCore.SharedCredentials(key);
 
@@ -14,29 +15,26 @@ let clientBuilder = new SmartyCore.ClientBuilder(credentials).withLicenses(["int
 let client = clientBuilder.buildInternationalAddressAutocompleteClient();
 
 // Documentation for input fields can be found at:
-//www.smarty.com/docs/cloud/international-address-autocomplete-api#pro-http-request-input-fields
+// www.smarty.com/docs/cloud/international-address-autocomplete-api#pro-http-request-input-fields
 
 let lookup = new Lookup({search: "Louis", country: "FRA"});
 
-(async () => {
-	console.log("Example request using the first result address_id in each subsequent request until results are refined to a single suggestion.");
-	console.log("*".repeat(20) + "\n");
+console.log("Example request using the first result address_id in each subsequent request until results are refined to a single suggestion.");
+console.log("*".repeat(20) + "\n");
 
-	console.log("Performing simple request...");
-	lookup = await handleRequest(lookup);
-	logSuggestions(lookup);
-	lookup.address_id = lookup.result[0].addressId;
+console.log("Performing simple request...");
+lookup = await handleRequest(lookup);
+logSuggestions(lookup);
+lookup.address_id = lookup.result[0].addressId;
 
-	console.log("Performing new request with refined address_id");
-	lookup = await handleRequest(lookup);
-	logSuggestions(lookup);
-	lookup.address_id = lookup.result[0].addressId;
+console.log("Performing new request with refined address_id");
+lookup = await handleRequest(lookup);
+logSuggestions(lookup);
+lookup.address_id = lookup.result[0].addressId;
 
-	console.log("Performing new request with refined address_id");
-	lookup = await handleRequest(lookup);
-	console.log("final result refined to address data: ", lookup.result);
-})();
-
+console.log("Performing new request with refined address_id");
+lookup = await handleRequest(lookup);
+console.log("final result refined to address data: ", lookup.result);
 
 function logSuggestions(response) {
 	console.log(response.result);
