9ac13d6518f227ece4fccbd706c84cb28341130e
Cami Blanch <caminblanch@gmail.com>
2023-10-31 15:16:22 -0600

    Renames to urlParams

--- src/Request.js
+++ src/Request.js
@@ -1,7 +1,7 @@
 class Request {
 	constructor(payload) {
 		this.baseUrl = "";
-		this.addressId = "";
+		this.urlParams = "";
 		this.payload = payload;
 		this.headers = {
 			"Content-Type": "application/json; charset=utf-8",
