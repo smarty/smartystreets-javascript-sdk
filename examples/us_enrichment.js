9af3bac437c0f5cbd1d447ddc8ec4e206005ca45
duncan <duncan@smarty.com>
2024-03-25 11:41:56 -0600

    Completed us_enrichment example.

---
+++ examples/us_enrichment.js
@@ -0,0 +1,34 @@
+const SmartySDK = require("smartystreets-javascript-sdk");
+const SmartyCore = SmartySDK.core;
+const Lookup = SmartySDK.usEnrichment.Lookup;
+
+// for Server-to-server requests, use this code:
+// let authId = process.env.SMARTY_AUTH_ID;
+// let authToken = process.env.SMARTY_AUTH_TOKEN;
+// const credentials = new SmartyCore.StaticCredentials(authId, authToken);
+
+// for client-side requests (browser/mobile), use this code:
+let key = process.env.SMARTY_EMBEDDED_KEY;
+const credentials = new SmartyCore.SharedCredentials(key);
+
+// The appropriate license values to be used for your subscriptions
+// can be found on the Subscription page of the account dashboard.
+// https://www.smarty.com/docs/cloud/licensing
+let clientBuilder = new SmartyCore.ClientBuilder(credentials).withLicenses(["us-property-data-principal-cloud"]);
+let client = clientBuilder.buildUsEnrichmentClient();
+
+// Documentation for input fields can be found at:
+// https://www.smarty.com/docs/us-street-api#input-fields
+
+let lookup = new Lookup("334968275");
+
+handleResponse(lookup).then();
+
+async function handleResponse(lookup) {
+    try {
+        const result = await client.sendPrincipal(lookup);
+        console.log(result.response);
+    } catch(err) {
+        console.log(err);
+    }
+}
