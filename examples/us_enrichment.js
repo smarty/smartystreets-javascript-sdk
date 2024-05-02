a3d8fb8571a47c16f798a0e6a60255f8e1ca8924
Cami Blanch <caminblanch@gmail.com>
2024-05-02 10:55:53 -0600

    Swaps spaces for tabs for consistency

--- examples/us_enrichment.js
+++ examples/us_enrichment.js
@@ -15,7 +15,7 @@ const credentials = new SmartyCore.SharedCredentials(key);
 // can be found on the Subscription page of the account dashboard.
 // https://www.smarty.com/docs/cloud/licensing
 let clientBuilder = new SmartyCore.ClientBuilder(credentials).withLicenses(["us-property-data-principal-cloud"]);
-    // .withBaseUrl("YOUR URL") // withBaseUrl() should be used if you are self-hosting the Smarty API
+	// .withBaseUrl("YOUR URL") // withBaseUrl() should be used if you are self-hosting the Smarty API
 
 let client = clientBuilder.buildUsEnrichmentClient();
 
@@ -27,10 +27,10 @@ let lookup = new Lookup("334968275");
 handleResponse(lookup).then();
 
 async function handleResponse(lookup) {
-    try {
-        const result = await client.sendPrincipal(lookup);
-        console.log(result.response);
-    } catch(err) {
-        console.log(err);
-    }
+	try {
+		const result = await client.sendPrincipal(lookup);
+		console.log(result.response);
+	} catch (err) {
+		console.log(err);
+	}
 }
