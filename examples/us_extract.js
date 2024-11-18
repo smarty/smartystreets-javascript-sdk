bd3e35eac04e48ff7d05571571f09c14d6e023e4
Max Pothier <max@smarty.com>
2024-11-18 16:00:10 -0700

    custom parameters for extract api

--- examples/us_extract.js
+++ examples/us_extract.js
@@ -1,15 +1,11 @@
-const SmartySDK = require("smartystreets-javascript-sdk");
+const SmartySDK = require("../dist/cjs/index.cjs");
 const SmartyCore = SmartySDK.core;
 const Lookup = SmartySDK.usExtract.Lookup;
 
 // for Server-to-server requests, use this code:
-// let authId = process.env.SMARTY_AUTH_ID;
-// let authToken = process.env.SMARTY_AUTH_TOKEN;
-// const credentials = new SmartyCore.StaticCredentials(authId, authToken);
-
-// for client-side requests (browser/mobile), use this code:
-let key = process.env.SMARTY_EMBEDDED_KEY;
-const credentials = new SmartyCore.SharedCredentials(key);
+let authId = process.env.SMARTY_AUTH_ID;
+let authToken = process.env.SMARTY_AUTH_TOKEN;
+const credentials = new SmartyCore.StaticCredentials(authId, authToken);
 
 let clientBuilder = new SmartyCore.ClientBuilder(credentials);
 	// .withBaseUrl("YOUR URL") // withBaseUrl() should be used if you are self-hosting the Smarty API
@@ -19,12 +15,20 @@ let client = clientBuilder.buildUsExtractClient();
 // Documentation for input fields can be found at:
 // https://www.smarty.com/docs/cloud/us-extract-api#http-request-input-fields
 
-let lookup = new Lookup("If you work at 1600 Pennsylvania Ave NW, Washington DC you're gonna have a hard time.");
+let lookup = new Lookup(`There are addresses everywhere.
+		1109 Ninth 85007
+		Smarty can find them.
+		3785 Las Vegs Av.
+		Los Vegas, Nevada
+		That is all.`);
 lookup.aggressive = true;
-lookup.addressesHaveLineBreaks = false;
 lookup.addressesPerLine = 1;
 
-await handleRequest(lookup);
+lookup.addCustomParameter("addr_line_breaks", false);
+
+(async () => {
+	await handleRequest(lookup);
+})();
 
 function logResult(response) {
 	console.log(response.result);
@@ -32,7 +36,7 @@ function logResult(response) {
 
 async function handleRequest(lookup) {
 	try {
-		const response = await client.send(lookup);
+		const response = await client.send(lookup); 
 		logResult(response);
 	} catch(err) {
 		console.log(err);
