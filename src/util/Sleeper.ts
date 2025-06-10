9b32a96a10f7190ec78b39d4f85e6d8bb00188e1
Andrew Blonquist <andrew.blonquist@smarty.com>
2025-06-10 09:16:20 -0600

    removes type and unused library

--- src/util/Sleeper.ts
+++ src/util/Sleeper.ts
@@ -1,6 +1,6 @@
 export default class Sleeper {
 	constructor () {}
 	sleep(seconds: number) {
-		return new Promise<void>((resolve: () => void)  => setTimeout(resolve, seconds*1000));
+		return new Promise<void>((resolve)  => setTimeout(resolve, seconds*1000));
 	}
 }
