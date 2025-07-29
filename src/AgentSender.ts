1a1722f686b183de6ec4ff5694eb77f491ea4394
Andrew Blonquist <andrew.blonquist@smarty.com>
2025-07-29 12:03:44 -0600

    another try

--- src/AgentSender.ts
+++ src/AgentSender.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Sender } from "./types";
+import packageJson from "../package.json" with { type: "json" };
 
 export default class AgentSender {
 	private sender: Sender;
@@ -9,7 +10,7 @@ export default class AgentSender {
 
 	send(request: Request): Promise<Response> {
 		request.parameters["agent"] =
-			"smarty (sdk:javascript@" + require("../../package.json").version + ")";
+			"smarty (sdk:javascript@" + packageJson.version + ")";
 		return new Promise((resolve, reject) => {
 			this.sender.send(request).then(resolve).catch(reject);
 		});
