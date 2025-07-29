36093d89bc99c475263a135169b7891844a49f25
Andrew Blonquist <andrew.blonquist@smarty.com>
2025-07-29 11:59:44 -0600

    fixes error

--- tests/test_AgentSender.ts
+++ tests/test_AgentSender.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import * as SmartySDK from "../dist/esm/index.mjs";
-import packageJson from "../package.json";
+import packageJson from "../package.json" with { type: "json" };
 
 describe("An agent sender", function () {
 	it("attaches an 'agent' parameter to the request config.", function () {
