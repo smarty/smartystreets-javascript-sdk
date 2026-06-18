902a309b829ca8bdb62c46e0d317b673de89cccb
andrea-wait-smarty <andrea.wait@smarty.com>
2026-06-18 14:23:15 -0600

    Change source field to const value (#148)

--- index.ts
+++ index.ts
@@ -53,7 +53,8 @@ export type { Request, Response, Sender, Sleeper, Signer } from "./src/types.js"
 export { SmartyError, NotModifiedError } from "./src/Errors.js";
 export type { MatchStrategy, OutputFormat, CountySource } from "./src/us_street/Lookup.js";
 export type { CoordinateLicense, MatchInfo } from "./src/us_street/Candidate.js";
-export type { Geolocation } from "./src/us_autocomplete_pro/Lookup.js";
+export type { Geolocation, AutocompleteSource } from "./src/us_autocomplete_pro/Lookup.js";
+export type { ReverseGeoSource } from "./src/us_reverse_geo/Lookup.js";
 export type { Language, Geocode } from "./src/international_street/Lookup.js";
 
 export {
